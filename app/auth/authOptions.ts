3b55d665e46f1a7e7519f94f4c4310beb3054c6a
Scorpion_King <pateljenny555@gmail.com>
2024-03-17 10:35:50 -0700

    Setting up drizzle adapter and db

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,13 +1,20 @@
+import { db } from "@/drizzle";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { NextAuthOptions } from "next-auth";
+import type { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 
 const authOptions: NextAuthOptions = {
+  adapter: DrizzleAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 };
 
 export default authOptions;
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -5,7 +5,10 @@ import {
   text,
   timestamp,
   uuid,
+  primaryKey,
+  integer,
 } from "drizzle-orm/pg-core";
+import type { AdapterAccount } from "@auth/core/adapters";
 
 export const tasks = pgTable("tasks", {
   id: uuid("id").primaryKey(),
@@ -32,3 +35,67 @@ export const tasks_labels = pgTable("tasks_labels", {
   task_id: uuid("task_id").references(() => tasks.id),
   label_id: uuid("label_id").references(() => labels.id),
 });
+
+// OAuth User
+export const users = pgTable("user", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name"),
+  email: text("email").notNull(),
+  emailVerified: timestamp("emailVerified", {
+    mode: "date",
+  }),
+  image: text("image"),
+});
+
+export const accounts = pgTable(
+  "account",
+  {
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type")
+      .$type<AdapterAccount["type"]>()
+      .notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [
+        account.provider,
+        account.providerAccountId,
+      ],
+    }),
+  })
+);
+
+export const sessions = pgTable("session", {
+  sessionToken: text("sessionToken").notNull().primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: timestamp("expires", { mode: "date" }).notNull(),
+});
+
+export const verificationTokens = pgTable(
+  "verificationToken",
+  {
+    identifier: text("identifier").notNull(),
+    token: text("token").notNull(),
+    expires: timestamp("expires", {
+      mode: "date",
+    }).notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({
+      columns: [vt.identifier, vt.token],
+    }),
+  })
+);
