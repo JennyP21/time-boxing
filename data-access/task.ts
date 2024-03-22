8dca801150e2898319c2d6dcc0d0e7c418914b52
Scorpion_King <pateljenny555@gmail.com>
2024-03-22 16:29:09 -0700

    created db access for tasks

---
+++ data-access/task.ts
@@ -0,0 +1,51 @@
+import { db } from "@/drizzle";
+import { tasks } from "@/drizzle/schema";
+import { Task } from "@/interfaces";
+import { eq } from "drizzle-orm";
+
+export async function getTasks() {
+  const tasks = await db.query.tasks.findMany();
+
+  return tasks;
+}
+
+export async function getTasksByBucket(bucket_id: string) {
+  const tasksByBucket = await db
+    .select()
+    .from(tasks)
+    .where(eq(tasks.bucket_id, bucket_id));
+
+  return tasksByBucket;
+}
+
+export async function addTask(task: Task) {
+  const newTask = await db
+    .insert(tasks)
+    .values(task)
+    .returning();
+
+  return newTask;
+}
+
+export async function getTask(id: string) {
+  const task = await db
+    .select()
+    .from(tasks)
+    .where(eq(tasks.id, id));
+
+  return task;
+}
+
+export async function deleteTask(id: string) {
+  await db.delete(tasks).where(eq(tasks.id, id));
+}
+
+export async function updateTask(id: string, task: Task) {
+  const updatedTask = await db
+    .update(tasks)
+    .set(task)
+    .where(eq(tasks.id, id))
+    .returning();
+
+  return updatedTask;
+}
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -5,3 +5,21 @@ export interface Bucket {
   created_at: Date;
   updated_at: Date;
 }
+
+export interface Task {
+  id: string;
+  user_id: string;
+  bucket_id: string;
+  title: string;
+  start_date: string;
+  end_date: string;
+  severity: "low" | "medium" | "high" | "urgent";
+  progress:
+    | "Not Started"
+    | "In Progress"
+    | "On Hold"
+    | "Completed";
+  steps: string[];
+  created_at: Date;
+  updated_at: Date;
+}
