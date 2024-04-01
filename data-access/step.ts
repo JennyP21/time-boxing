fd0fae31d8fb45e9a9671eea7f672a693b2cdef7
Scorpion_King <pateljenny555@gmail.com>
2024-03-31 18:26:47 -0700

    Created steps data access

--- data-access/step.ts
+++ data-access/step.ts
@@ -1,5 +1,6 @@
 import { db } from "@/drizzle";
 import { steps } from "@/drizzle/schema";
+import { StepsI } from "@/interfaces";
 import { eq } from "drizzle-orm";
 
 export async function getStepsByTaskId(task_id: string) {
@@ -9,4 +10,29 @@ export async function getStepsByTaskId(task_id: string) {
     .where(eq(steps.task_id, task_id));
 
   return result;
+}
+
+export async function addStep(step: StepsI) {
+  const newStep = await db
+    .insert(steps)
+    .values(step)
+    .returning();
+
+  return newStep;
+}
+
+export async function updateStep(id: string, step: StepsI) {
+  const updatedStep = await db
+    .update(steps)
+    .set(step)
+    .where(eq(steps.id, id))
+    .returning();
+
+  return updatedStep;
+}
+
+export async function deleteStep(id: string) {
+  await db.delete(steps).where(eq(steps.id, id));
+
+  return [];
 }
