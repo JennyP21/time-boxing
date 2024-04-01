8faf9d3277877449fd23ee58b05f99ab2bf50958
Scorpion_King <pateljenny555@gmail.com>
2024-04-01 12:04:23 -0700

    Handling label assign and unassign ops

--- components/ui/LabelDetails.tsx
+++ components/ui/LabelDetails.tsx
@@ -2,11 +2,10 @@
 import Skeleton from "@/components/ui/Skeleton";
 import { LabelI, Task_LabelI } from "@/interfaces";
 import { useAssignLabelMutation, useGetLabelsByTaskQuery, useGetLabelsQuery, useUnassignLabelMutation, useUpdateLabelMutation } from '@/lib/features/labelApi';
-import { Box, Flex, Icon, Input, InputGroup, InputRightElement, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { Flex, Icon, Input, InputGroup, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import { SyntheticEvent, useState } from "react";
 import { IoClose } from "react-icons/io5";
 import { MdOutlineNewLabel } from "react-icons/md";
-
 
 interface Props {
     task_id: string;
@@ -15,6 +14,7 @@ interface Props {
 const LabelDetails = ({ task_id }: Props) => {
     const { data: labels } = useGetLabelsByTaskQuery(task_id);
     const { data: allLabels, isLoading } = useGetLabelsQuery();
+    const filteredLabels = allLabels?.filter(label => !labels?.find(item => item.id === label.id));
 
     const initialState = {
         labelId: "",
@@ -29,64 +29,64 @@ const LabelDetails = ({ task_id }: Props) => {
         const currentLabel = labels?.find(label => label.id === editLabel.labelId);
         if (currentLabel?.name !== newValue) {
             const newLabel = {
-                ...currentLabel,
+                id: currentLabel?.id,
                 name: newValue
             } as LabelI;
+            console.log(newLabel);
             await updateLabel(newLabel);
         }
         setEditLabel(initialState);
     }
 
     const [assignLabel] = useAssignLabelMutation();
-    const handleLabelAssignment = async () => {
+    const handleLabelAssignment = async (label_id: string) => {
         const data = {
             task_id,
-            label_id: editLabel.labelId
+            label_id,
         } as Task_LabelI;
         await assignLabel(data);
     }
 
     const [unassignLabel] = useUnassignLabelMutation();
-    const handleLabelUnAssignment = async () => {
+    const handleLabelUnAssignment = async (label_id: string) => {
         const data = {
             task_id,
-            label_id: editLabel.labelId
+            label_id,
         } as Task_LabelI;
         await unassignLabel(data)
-
-        setEditLabel(initialState);
     }
 
     return (
         <Flex className='my-2'>
-            <Menu>
+            {filteredLabels && filteredLabels?.length > 0 && <Menu>
                 <MenuButton>
                     <Icon as={MdOutlineNewLabel} w={6} h={6} mr={2} />
                 </MenuButton>
                 <MenuList>
-                    {allLabels?.map(label => (
-                        <MenuItem key={label.id}>{label.name}</MenuItem>
+                    {filteredLabels.map(label => (
+                        <MenuItem key={label.id} onClick={() => handleLabelAssignment(label.id)}>{label.name}</MenuItem>
                     ))}
                     {isLoading && <Skeleton count={3} width="100%" height="25px" />}
                 </MenuList>
-            </Menu>
+            </Menu>}
             <Flex gap={2}>
                 {labels?.map((label) => (
-                    <>
+                    <Flex key={label.id} alignContent="center">
                         {editLabel.editing && label.id === editLabel.labelId ?
-                            <InputGroup key={label.id}>
+                            <InputGroup>
                                 <Input autoFocus className='!w-fit !h-fit rounded-md !text-xs !p-1' bg={"gray.300"} defaultValue={label.name} onBlur={(e) => handleLabelUpdate(e)} />
-                                <InputRightElement className="rounded-full !w-fit !h-fit !top-[50%] -translate-y-[50%] !right-1" _hover={{
-                                    bg: "gray.400"
-                                }} children={<IoClose />} onClick={() => setEditLabel(initialState)} />
                             </InputGroup>
                             :
-                            <Box as={"span"} className='rounded-md text-xs p-1' bg={"gray.300"} key={label.id} onClick={() => setEditLabel({
+                            <Flex as={"span"} className='items-center rounded-md text-xs p-1 gap-1' bg={"gray.300"} onClick={() => setEditLabel({
                                 labelId: label.id,
                                 editing: true
-                            })}>{label.name}</Box>
+                            })}>{label.name}
+                                <Icon as={IoClose} className="rounded-full text-md" w={4} h={4} _hover={{
+                                    bg: "gray.400"
+                                }} onClick={() => handleLabelUnAssignment(label.id)} />
+                            </Flex>
                         }
-                    </>
+                    </Flex>
                 ))}
             </Flex>
         </Flex>
