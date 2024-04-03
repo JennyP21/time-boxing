999ef66422a886ba4b104c0c0aa2c07c71e60cf7
Scorpion_King <pateljenny555@gmail.com>
2024-04-02 17:52:18 -0700

    Created active state in left panel

--- app/LeftPanel.tsx
+++ app/LeftPanel.tsx
@@ -1,10 +1,13 @@
 "use client";
-import { Flex, Icon, Link, Text, VStack } from '@chakra-ui/react';
+import { Icon, Link, Text, VStack } from '@chakra-ui/react';
+import { usePathname } from 'next/navigation';
 import { BsKanban } from "react-icons/bs";
 import { FaRegListAlt } from "react-icons/fa";
 import { IoHomeOutline } from "react-icons/io5";
 
 const LeftPanel = () => {
+
+    const path = usePathname();
 
     const panelItems = [
         {
@@ -32,16 +35,14 @@ const LeftPanel = () => {
             py={1}
             bg="white"
         >
-            <Flex className='whitespace-nowrap w-full' flexDir="column" gap={4}>
-                {panelItems.map(item => (
-                    <Link href={item.href} key={item.name}>
-                        <VStack _hover={{ bg: "gray.50" }} rounded="5px" spacing={2} p={1} cursor='pointer'>
-                            <Icon as={item.icon} w={7} h={7} />
-                            <Text fontSize='x-small'>{item.name}</Text>
-                        </VStack>
-                    </Link>
-                ))}
-            </Flex>
+            {panelItems.map(item => (
+                <Link href={item.href} key={item.name}>
+                    <VStack _hover={{ bg: "gray.50" }} rounded="5px" spacing={2} p={1} cursor='pointer'>
+                        <Icon as={item.icon} w={7} h={7} boxShadow={path === item.href ? "2xl" : ""} />
+                        <Text fontSize='x-small' textDecor={path === item.href ? "underline" : ""}>{item.name}</Text>
+                    </VStack>
+                </Link>
+            ))}
         </VStack>
     )
 }
