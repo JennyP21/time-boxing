4d5a9472c8c1317486261a8e22b802bfc027056a
Scorpion_King <pateljenny555@gmail.com>
2024-04-06 14:23:47 -0700

    fixed location not defined issue.

--- app/kanban/KanbanCanvas.tsx
+++ app/kanban/KanbanCanvas.tsx
@@ -4,17 +4,21 @@ import GroupByProgress from '@/components/ui/Progress/GroupByProgress';
 import GroupBySeverity from '@/components/ui/Severity/GroupBySeverity';
 import { groupTypes } from '@/constants';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 import GroupByBucket from '../../components/ui/Bucket/GroupByBucket';
 
 const KanbanCanvas = () => {
     const router = useRouter();
     const searchParams = useSearchParams();
     let groupBy = searchParams.get("groupBy");
-    if (!groupBy) {
-        const params = new URLSearchParams(searchParams);
-        params.set("groupBy", "Bucket");
-        router.push("?" + params);
-    }
+
+    useEffect(() => {
+        if (!groupBy) {
+            const params = new URLSearchParams(searchParams);
+            params.set("groupBy", "Bucket");
+            router.push("?" + params);
+        }
+    }, [])
     if (groupBy && !groupTypes.includes(groupBy)) {
         return null;
     }
