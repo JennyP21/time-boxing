411e83b6196c52957bf3cf94deec84056b7dc04a
Scorpion_King <pateljenny555@gmail.com>
2024-04-24 18:42:53 -0700

    created fallback image for users profile

--- components/ui/NavLinks.tsx
+++ components/ui/NavLinks.tsx
@@ -1,18 +1,18 @@
 import { Button, Center, Link, Menu, MenuButton, MenuItem, MenuList, Stack } from '@chakra-ui/react';
+import { Session } from 'next-auth';
 import Image from 'next/image';
 
-const NavLinks = ({ session }: { session: any }) => {
-
+const NavLinks = ({ session }: { session: Session | null }) => {
     return (
         <>
             {session ?
                 <Center>
                     <Menu>
                         <MenuButton as={Button} borderRadius="50%" colorScheme='gray' minWidth={{ base: 10 }} minHeight={{ base: 10 }} overflow={'hidden'}>
-                            <Image src={session.user.image} fill className='object-cover' alt="profile" />
+                            <Image src={session.user.image || "/fallback-user.webp"} fill className='object-cover' alt="profile" />
                         </MenuButton>
                         <MenuList fontSize={{ sm: "medium", base: "small" }}>
-                            <MenuItem >Profile</MenuItem>
+                            <MenuItem>Profile</MenuItem>
                             <MenuItem>Settings</MenuItem>
                             <MenuItem href='/api/auth/signout' as={'a'}>Logout</MenuItem>
                         </MenuList>
