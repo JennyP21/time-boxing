8efa1fb0fe67d485db6b53f946ecc15cd93837ef
Scorpion_King <pateljenny555@gmail.com>
2024-05-05 13:19:01 -0700

    Added link and color coding for tasks by due date

--- app/(main)/dashboard/TasksByDueDate.tsx
+++ app/(main)/dashboard/TasksByDueDate.tsx
@@ -1,12 +1,14 @@
 import { TaskI } from '@/interfaces';
 import { Flex, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 import ChartHeading from './ChartHeading';
+import { Link } from '@chakra-ui/next-js';
 
 interface Props {
     tasks: TaskI[];
 }
 
 const TasksByDueDate = ({ tasks }: Props) => {
+    const today = new Date();
     return (
         <Flex className='flex-col rounded-lg' width={300} border='1px' borderColor='gray.300'>
             <ChartHeading>Top Upcoming tasks</ChartHeading>
@@ -20,8 +22,12 @@ const TasksByDueDate = ({ tasks }: Props) => {
                 <Tbody>
                     {tasks.map(task => (
                         <Tr key={task.id}>
-                            <Td p={3} textAlign="start">{task.title}</Td>
-                            <Td p={3} textAlign="start">{task.end_date}</Td>
+                            <Td p={3} textAlign="start">
+                                <Link href={`/project/${task.project_id}`}>
+                                    {task.title}
+                                </Link>
+                            </Td>
+                            <Td p={3} textAlign="start" color={today > new Date(task.end_date) ? "red.500" : "black"}>{task.end_date}</Td>
                         </Tr>
                     ))}
                 </Tbody>
