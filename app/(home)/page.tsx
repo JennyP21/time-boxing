44826c0f4e03f047715427a4e532222c3824e244
Scorpion_King <pateljenny555@gmail.com>
2024-05-06 19:44:34 -0700

    Created home page group

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import { Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <Flex className='flex-col mt-16 mx-[20%]'>
+            Home
+        </Flex>
+    )
+}
+
+export default Home
