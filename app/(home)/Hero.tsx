f5062eec7ba8aeb25e042ae6054a88fd7b1d96d0
Scorpion_King <pateljenny555@gmail.com>
2024-05-11 15:04:52 -0700

    refactor hero

--- app/(home)/Hero.tsx
+++ app/(home)/Hero.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 const Hero = () => {
     return (
         <Grid
-            className='flex-col w-full py-16 px-32 items-center justify-center'
+            className='flex-col w-full py-20 px-32 items-center justify-center'
             templateColumns={{ sm: "1fr", md: '1fr 1fr' }}
-            backgroundColor="teal.400"
+            backgroundColor="blue.400"
             style={{
                 clipPath: 'polygon(0 0, 100% 0, 100% 85%, 85% 100%, 50% 90%, 15% 100%, 0 85%, 0% 30%)'
             }}
