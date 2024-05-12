5e381554152763e37c3107467078c6451222a844
Scorpion_King <pateljenny555@gmail.com>
2024-05-12 12:53:45 -0700

    Changed order or home components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -12,9 +12,9 @@ const Home = () => {
             <Divider />
             <Features />
             <Divider />
-            <Reviews />
-            <Divider />
             <Price />
+            <Divider />
+            <Reviews />
             <Divider />
             <Footer />
         </Flex>
