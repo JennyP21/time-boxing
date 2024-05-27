50b6076fd76706173ec91aafbcf2d318576056ce
Scorpion_King <pateljenny555@gmail.com>
2024-05-26 17:55:23 -0700

    Refactor features page and images

--- app/(content)/(home)/Features.tsx
+++ app/(content)/(home)/Features.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { featureDesc, featureIntro, features } from '@/constants'
-import { Box, Card, CardBody, CardHeader, Flex, Heading, SimpleGrid, Text } from '@chakra-ui/react'
+import { Box, Card, CardBody, CardHeader, Flex, Grid, Heading, Text } from '@chakra-ui/react'
 import Image from 'next/image'
 import { useState } from 'react'
 
@@ -9,46 +9,44 @@ const Features = () => {
     const active = allFeatures.find(feature => feature.active);
 
     const handleChange = (selectedFeatureHeader: string) => {
-        const selectedFeature = allFeatures.find(feature => feature.header === selectedFeatureHeader);
-        const activeFeature = allFeatures.find(feature => feature.active);
-        if (selectedFeature && activeFeature && selectedFeature.header !== activeFeature.header) {
-            setAllFeatures([
-                ...allFeatures.map(feature => feature.header === selectedFeature.header
-                    ? { ...selectedFeature, active: true }
-                    : feature.header === activeFeature.header ? { ...activeFeature, active: false } : feature
-                )
-            ])
-        }
+        setAllFeatures([
+            ...allFeatures.map(feature => feature.header === selectedFeatureHeader
+                ? { ...feature, active: true }
+                : { ...feature, active: false }
+            )
+        ])
     }
 
     return (
-        <Flex className='py-10 px-40 flex-col justify-center gap-3'>
+        <Flex className='py-10 px-0 md:px-10 lg:px-20 xl:px-40 flex-col justify-center gap-3'>
             <Heading pl={3} size="lg" fontWeight="semibold">{featureIntro}</Heading>
             <Text pl={3} maxW={600}>
                 {featureDesc}
             </Text>
-            <SimpleGrid className='items-center justify-center gap-5 my-2 mx-4' templateColumns="1fr 2fr">
-                <Flex className='flex-col gap-5'>
+            <Grid
+                className='items-center justify-center gap-3 my-2 mx-4'
+                gridTemplateColumns={{ base: "1fr", lg: "1fr 2fr" }}
+            >
+                <Flex className='gap-2 w-full' direction={{ base: "row", lg: "column" }}>
                     {allFeatures.map((feature, index) => (
                         <Card
-                            className={feature.active ? "!border-b-4" : ""}
+                            className={feature.active ? "!border-b-8" : ""}
                             cursor="pointer"
                             shadow={feature.active ? "0 0 20px 1px #d0cfcf" : "none"}
                             size="sm"
                             key={index}
-                            maxW={400}
                             borderColor="blue.300"
                             onClick={() => handleChange(feature.header)}
                         >
                             <CardHeader p={3} fontWeight="bold">{feature.header}</CardHeader>
-                            <CardBody p={3} fontSize="small">{feature.body}</CardBody>
+                            <CardBody p={3} fontSize="smaller">{feature.body}</CardBody>
                         </Card>
                     ))}
                 </Flex>
                 <Box className='mx-auto'>
-                    {active && <Image src={active.image} width={700} height={417} alt={active.linkText} />}
+                    {active && <Image src={active.image} width={800} height={417} alt={active.linkText} />}
                 </Box>
-            </SimpleGrid>
+            </Grid>
         </Flex>
     )
 }
