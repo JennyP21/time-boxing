5941213187952a34d781da83c80e59d4b86bc4d9
Scorpion_King <pateljenny555@gmail.com>
2024-05-27 18:10:19 -0700

    Fixed missing navlinks

--- components/ui/NavLinksWithUser.tsx
+++ components/ui/NavLinksWithUser.tsx
@@ -14,28 +14,29 @@ const NavLinksWithUser = ({ email }: { email: string }) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const { data: user, error, isLoading } = useGetUserByIdQuery(email);
     if (error) handleErrors(error, getUserError.type);
-    if (!user) return null;
 
     return (
         <>
             {isLoading ?
                 <NavLinksLoading />
                 :
-                <HStack>
-                    <Button colorScheme='blue' variant="outline" size={{ base: "xs", sm: "sm" }}>
-                        <Link href={currPath === '/' ? '/dashboard' : '/'}>{currPath === '/' ? 'Dashboard' : 'Home'}</Link>
-                    </Button>
-                    <Menu>
-                        <MenuButton as={Button} borderRadius="50%" colorScheme='gray' minWidth={10} minHeight={10} overflow={'hidden'}>
-                            <Image src={user.image || "/fallback-user.webp"} fill className='object-cover' alt="profile" />
-                        </MenuButton>
-                        <MenuList fontSize={{ sm: "medium", base: "small" }}>
-                            <MenuItem onClick={onOpen}>Profile</MenuItem>
-                            <MenuItem href='/api/auth/signout' as={'a'}>Logout</MenuItem>
-                        </MenuList>
-                    </Menu>
-                    <Profile user={user} isOpen={isOpen} onClose={onClose} />
-                </HStack>}
+                (user &&
+                    <HStack>
+                        <Button colorScheme='blue' variant="outline" size={{ base: "xs", sm: "sm" }}>
+                            <Link href={currPath === '/' ? '/dashboard' : '/'}>{currPath === '/' ? 'Dashboard' : 'Home'}</Link>
+                        </Button>
+                        <Menu>
+                            <MenuButton as={Button} borderRadius="50%" colorScheme='gray' minWidth={10} minHeight={10} overflow={'hidden'}>
+                                <Image src={user.image || "/fallback-user.webp"} fill className='object-cover' alt="profile" />
+                            </MenuButton>
+                            <MenuList fontSize={{ sm: "medium", base: "small" }}>
+                                <MenuItem onClick={onOpen}>Profile</MenuItem>
+                                <MenuItem href='/api/auth/signout' as={'a'}>Logout</MenuItem>
+                            </MenuList>
+                        </Menu>
+                        <Profile user={user} isOpen={isOpen} onClose={onClose} />
+                    </HStack>
+                )}
         </>
     )
 }
